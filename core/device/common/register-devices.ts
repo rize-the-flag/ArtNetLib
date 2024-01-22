a7ec26269ec196d107a549aebb7a84fc38ecb0d4
rize-the-flag <dev.denism@yandex.ru>
2024-01-22 23:16:14 +0600

    cleanup

--- core/device/common/register-devices.ts
+++
@@ -1,8 +0,0 @@
-import {DeviceFactory} from "./factory";
-import {MixPanel150} from "../MixPanel150/mix-panel150";
-import {DummyDevice} from "../dummy/dummy";
-
-//DMX devices that are implemented natively in library registered here
-DeviceFactory.registerDevice("MixPanel150", MixPanel150);
-DeviceFactory.registerDevice('Dummy', DummyDevice);
-
--- core/device/dummy/types.ts
+++
@@ -1,5 +0,0 @@
-export type DummyDeviceApi = {
-    setChannels: {
-        channels: number[]
-    }
-}
